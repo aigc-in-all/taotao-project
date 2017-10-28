ec435ed506ec465a7a042735847fb7a118cf7ce2
He Qingbao <qingbao.ho@gmail.com>
2017-10-28 17:34:25 +0800

    7.添加redis缓存

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ContentServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ContentServiceImpl.java
@@ -3,9 +3,11 @@ package com.taotao.service.impl;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.service.ContentService;
@@ -16,11 +18,25 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper contentMapper;
 
+    @Value("${REST_BASE_URL}")
+    private String REST_BASE_URL;
+
+    @Value("${REST_CONTENT_SYNC_URL}")
+    private String REST_CONTENT_SYNC_URL;
+
     @Override
     public TaotaoResult insertContent(TbContent content) {
         content.setCreated(new Date());
         content.setUpdated(new Date());
         contentMapper.insert(content);
+
+        // 刷新redis
+        try {
+            HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return TaotaoResult.ok();
     }
 
--- taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
+++ taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
@@ -3,12 +3,16 @@ package com.taotao.rest.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.util.StringUtil;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
 import com.taotao.pojo.TbContentExample.Criteria;
+import com.taotao.rest.dao.JedisClient;
 import com.taotao.rest.service.ContentService;
 
 @Service
@@ -17,12 +21,38 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper contentMapper;
 
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${INDEX_CONTENT_REDIS_KEY}")
+    private String INDEX_CONTENT_REDIS_KEY;
+
     @Override
     public List<TbContent> getContentList(Long categoryId) {
+        // 检查缓存
+        try {
+            String str = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, String.valueOf(categoryId));
+            if (StringUtil.isNotEmpty(str)) {
+                List<TbContent> contents = JsonUtils.jsonToList(str, TbContent.class);
+                return contents;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         TbContentExample example = new TbContentExample();
         Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(categoryId);
         List<TbContent> contents = contentMapper.selectByExample(example);
+
+        // 写入缓存
+        try {
+            String str = JsonUtils.objectToJson(contents);
+            jedisClient.hset(INDEX_CONTENT_REDIS_KEY, String.valueOf(categoryId), str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return contents;
     }
 
